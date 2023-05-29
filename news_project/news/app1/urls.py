6b3d44a048342e3ff6266af8e59530df888ee6aa
DK <darshangupta18gba17@gmail.com>
2023-05-29 20:13:17 +0530

    third_commit

--- news_project/news/app1/urls.py
+++ news_project/news/app1/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('bollywood/',views.bollywood,name='bollywood'),
+    path('hollywood/',views.hollywood,name='hollywood'),
+    path('political/',views.political,name='political'),
+    path('sports/',views.sports,name='sports'),
+    path('bussiness/',views.bussiness,name='bussiness'),
+    path('brand/',views.brand,name='brand')
 ]
--- news_project/news/app1/views.py
+++ news_project/news/app1/views.py
@@ -4,3 +4,22 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'home.html')
+
+def bollywood(request):
+    return render(request,'bollywood.html')
+
+def hollywood(request):
+    return render(request,'hollywood.html')
+
+def political(request):
+    return render(request,'political.html')
+
+def sports(request):
+    return render(request,'sports.html')
+
+def bussiness(request):
+    return render(request,'bussiness.html')
+
+def brand(request):
+    return render(request,'brand.html')
+
